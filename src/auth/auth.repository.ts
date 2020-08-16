f9ccb0bcc1307a2f4203d5d4a46e588b8beac8a5
baldore <acornssn@gmail.com>
2020-08-16 17:33:55 -0500

    Add hash strategy for password

--- src/auth/auth.repository.ts
+++ src/auth/auth.repository.ts
@@ -5,17 +5,22 @@ import {
 } from '@nestjs/common';
 import { AuthCredentialsDTO } from './dto/auth-credentials.dto';
 import { PrismaService } from 'src/persistence/prisma.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthRepository {
   constructor(private prisma: PrismaService) {}
 
   async createUser(credentials: AuthCredentialsDTO): Promise<void> {
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await this.hashPassword(credentials.password, salt);
+
     try {
       await this.prisma.user.create({
         data: {
           email: credentials.email,
-          password: credentials.password,
+          password: hashedPassword,
+          salt: salt,
         },
       });
     } catch (error) {
@@ -25,5 +30,9 @@ export class AuthRepository {
         throw new InternalServerErrorException();
       }
     }
+  }
+
+  async hashPassword(password: string, salt: string): Promise<string> {
+    return bcrypt.hash(password, salt);
   }
 }
